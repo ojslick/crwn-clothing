7023d2e6bdebec034d198c7f39153af32c218fcd
Oluwaseun Akogun <seun407@gmail.com>
2019-10-19 13:27:33 +0100

    added custom button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component';
+
 
 
 
@@ -16,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop/' component={ShopPage} />
+        <Route path='/signIn/' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
